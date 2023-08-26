109aeaa7ac8017520de3ff1f37c5b8063db09e8a
mechaneyes <ray@mechaneyes.com>
2023-08-25 20:58:43 -0400

    minute title tweak. added "|"
    + {`${post.title} | ${CMS_NAME}`}

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -33,7 +33,7 @@ export default function Post({ post, posts, preview }) {
             <article>
               <Head>
                 <title>
-                  {`${post.title} ${CMS_NAME}`}
+                  {`${post.title} | ${CMS_NAME}`}
                 </title>
                 <meta
                   property="og:image"
