bff2c87b47bacbeaa32bc20766e4c01f75584301
mechaneyes <ray@mechaneyes.com>
2023-11-25 23:19:23 -0800

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
-import '../styles/styles.scss'
+import { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
+
+import "../styles/index.css";
+import "../styles/styles.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
