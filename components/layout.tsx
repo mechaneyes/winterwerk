b7c40d6cae8076733102d9a5a5e5898ef9b02e9a
mechaneyes <ray@mechaneyes.com>
2023-12-01 20:36:34 -0800

    removed jquery/slick from layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,20 +2,8 @@ import Alert from "./alert";
 import Footer from "./footer";
 import Meta from "./meta";
 import Loader from "./loader";
-import $ from "jquery";
-import { useEffect } from "react";
 
 export default function Layout({ preview, children }) {
-  useEffect(() => {
-    $(document).ready(function () {
-      $('.simple-carousel').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-      });
-    });
-  });
-
   return (
     <>
       <Meta />
