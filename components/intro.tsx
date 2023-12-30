c84996a08fde398e267f57cd7d490b92c56e7015
mechaneyes <ray@mechaneyes.com>
2023-12-30 13:07:23 -0800

    styled header ° lotta other styling

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,7 +4,7 @@ export default function Intro() {
   return (
     <section className="home-header flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl md:pr-8">WinterWerk</h1>
-      <h4 className="">Reflections on art and tech via<br />Ray Weitzenberg</h4>
+      <h4 className="">Reflections on art and tech via Ray Weitzenberg</h4>
     </section>
   );
 }
